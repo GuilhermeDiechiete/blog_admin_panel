60a8d60f413f3c2de2f2a7c9eaea98270c12d033
Guilherme Diechiete <132406389+GuilhermeDiechiete@users.noreply.github.com>
2023-09-22 20:55:21 -0300

    create routes categories

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,30 @@
 const router = require('express').Router()
 
-router.get('/articles', (req, res) => {
-    res.send('Artigos')
+
+const Article = require('./ArticleModel')
+const slugify = require('slugify')
+
+const Category = require('../categories/CategoryModel')
+
+router.get('/admin/articles', (req, res) => {
+    res.render('admin/articles/list')
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('Criar novo artigo')
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
 })
+router.post('/admin/articles/save', (req, res) => {
+    const title = req.body.title
+    const body = req.body.body 
+    const category = req.body.category
 
+    Article.create({
+        title: title, slug: slugify(title), body: body, categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
+})
 
 module.exports = router
